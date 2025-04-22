62526de55aee614f2610754fe7345081e169587b
miokato <miokato@me.com>
2025-04-23 05:35:28 +0900

    Add Constants.

---
+++ HelloVisionOS/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  HelloVisionOS
+//
+//  Created by mio kato on 2025/04/20.
+//
+
+enum Constants {
+    static let volumetricWindowID = "01"
+    static let draggableBoxID = "02"
+}
